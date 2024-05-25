42afa7ab7333be5bea7e7369500b2b4617549f9a
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-05-25 23:18:09 +0530

    go routines

---
+++ cmd/tut9/main.go
@@ -0,0 +1,36 @@
+//Goroutines are a way to launch multiple functiosn and and have them execute concurrently
+
+//Concurrency is not same as parallel execution
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+var wg = sync.WaitGroup{}
+var m = sync.Mutex{}
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+var results = []string{}
+
+func main(){
+	t0 := time.Now()
+	for i:=0; i<len(dbData); i++{
+		wg.Add(1)
+		dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
+	fmt.Printf("\nThe results are %v", results)
+}
+
+func dbCall(i int) {
+	var delay float32 = 1000
+	time.Sleep(time.Duration(delay)*time.Millisecond)
+	fmt.Println("The result from the database is:", dbData[i])
+	m.Lock()
+	results = append(results, dbData[i])
+	m.Unlock()
+	wg.Done()
+}
