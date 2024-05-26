87f7c629d068e49e175c0d4fad0763fab4b2f321
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-05-26 22:07:45 +0530

    go routines concurrency

--- cmd/tut10/main.go
+++ cmd/tut10/main.go
@@ -1,7 +1,3 @@
-//Goroutines are a way to launch multiple functiosn and and have them execute concurrently
-
-//Concurrency is not same as parallel execution
-
 package main
 
 import (
@@ -10,7 +6,7 @@ import (
 	"time"
 )
 var wg = sync.WaitGroup{}
-var m = sync.Mutex{}
+var m = sync.RWMutex{}
 var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 var results = []string{}
 
@@ -24,14 +20,23 @@ func main(){
 	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
 	fmt.Printf("\nThe results are %v", results)
 }
-
 func dbCall(i int) {
 	var delay float32 = 1000
 	time.Sleep(time.Duration(delay)*time.Millisecond)
-	fmt.Println("The result from the database is:", dbData[i])
-	m.Lock()
-	results = append(results, dbData[i])
-	m.Unlock()
+	save(dbData[i])
+	log()
 	wg.Done()
 }
+func save(result string){
+	m.Lock()
+	results = append(results, result)
+	m.Unlock()
+}
 
+func log(){
+	m.RLock()
+	fmt.Printf("\n The current results are: %v\n", results)
+	m.RUnlock()
+}
+
+//This pattern allows multiple go routines to read forom our slice from same or only blocking or writes may be potentially happening 
