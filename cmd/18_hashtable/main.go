60eec7d76f65f09ccc7c69aaaf90f4649cd5e8d4
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2024-06-07 22:16:44 +0530

    june 7 hash table

--- cmd/18_hashtable/main.go
+++ cmd/18_hashtable/main.go
@@ -21,6 +21,75 @@ type bucketNode struct {
 	next *bucketNode
 }
 
+//Insert will take a kety and add it to the hash table array
+func (h *HashTable) Insert(key string) {
+	index:= hash(key)
+	h.array[index].insert(key)
+}
+
+
+
+// Search will take in a key and return true if that key is stored in the hash table
+func (h *HashTable) Search(key string) bool {
+	index:= hash(key)
+	return h.array[index].search(key)
+
+}
+
+//Delete will take in a key and delete it from the hash table
+func (h *HashTable) Delete(key string){
+	index:= hash(key)
+	h.array[index].delete(key)
+}
+
+//Bucket insert
+func (b *bucket) insert(k string){
+	if !b.search(k) {
+		newNode := &bucketNode{key: k}
+		newNode.next = b.head
+		b.head = newNode
+	} else {
+		fmt.Println(k, "Already Exists ")
+	}
+	
+}
+//Bucket search
+func (b *bucket) search(k string) bool {
+	currentNode := b.head
+	for currentNode != nil {
+		if currentNode.key == k {
+			return true
+		}
+		currentNode = currentNode.next
+	}
+	return false
+}
+
+//Bucket Delete
+func (b *bucket) delete(k string){
+	if b.head.key == k {
+		b.head =  b.head.next
+		return
+	}
+
+	previousNode := b.head
+	for previousNode.next != nil{
+		if previousNode.next.key == k {
+			previousNode.next = previousNode.next.next
+		}
+		previousNode = previousNode.next
+	}
+}
+
+func hash(key string) int {
+	sum := 0
+	for _,v := range key{
+		sum += int(v)
+	}
+	return sum % ArraySize
+}
+
+
 func Init() *HashTable{
 	result := &HashTable{}
 	
@@ -31,7 +100,23 @@ func Init() *HashTable{
 }
 
 func main(){
-	testHashTable := &HashTable{}
-	fmt.Println(testHashTable)
+	hashTable := Init()
 
+	list := []string{
+		"ERIC",
+		"BANA",
+		"KYLE",
+		"STAN",
+		"RANDY",
+		"TOKEN",
+	}
+	for _,v := range list{
+		hashTable.Insert(v)
+	}
+	fmt.Println(hashTable.Search("STAN"))
+	hashTable.Delete("STAN")
+	fmt.Println(hashTable.Search("STAN"))
+	fmt.Println(hashTable.Search("BANA")) 
+	fmt.Println(hashTable)
+	
 }
